17087049843989b7b61b9f1abb9cceb4a8be60d2
Axel <axel_cg4@hotmail.com>
2020-06-15 01:38:49 -0500

    ruleta deployed

---
+++ migrations/5_ruleta.js
@@ -0,0 +1,20 @@
+const CasinoRoulette = artifacts.require("CasinoRoulette");
+
+module.exports = function(deployer) {
+    deployer.deploy(CasinoRoulette);
+};
+
+
+/**
+ > transaction hash:    0x4ecdab6d67705b2528b7f670c782bc3af3306747ffdfcd2d463fb3780d841209
+   > Blocks: 1            Seconds: 24
+   > contract address:    0x7efd0Ca7Ec0e56b03E0081e8EFb6A705538D96a6
+   > block number:        8095865
+   > block timestamp:     1592202920
+   > account:             0xC1d89b29c3694F6c62B4a46C40DFA3E1854ffCC2
+   > balance:             12.2043546350009
+   > gas used:            2236471 (0x222037)
+   > gas price:           20 gwei
+   > value sent:          0 ETH
+   > total cost:          0.04472942 ETH
+ */
